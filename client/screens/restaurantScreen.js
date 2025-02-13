8cfe2fc8b57d084b74eb3320957ff6cac93a0a1b
User <User@WRSD-LD309-026.dut.ac.za>
2025-02-13 13:26:02 +0200

    screens updated

--- client/screens/restaurantScreen.js
+++ client/screens/restaurantScreen.js
@@ -1,5 +1,30 @@
-function RestaurantScreen() {
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { View, Text, Flatlist, Button } from "react-native"
 
+function RestaurantScreen() {
+ const [restuarants, setRestuarants] = useState([]);
+
+ useEffect(() => {
+    axios.get("http://zibusisonduna2001:8AsJgYl9K9R5N0rQ@cluster0/restaurants")
+     .then(response => setRestuarants(response.data))
+     .catch(error => console.log(error))
+ }, [])
+
+ return(
+    <View>
+        <Flatlist
+            data={restuarants}
+            renderItem={({item}) => (
+                <View>
+                    <Text>{item.name}</Text>
+                    <Button title="View Details" onPress={() => navigateToRestaurantDetails(item._id)} />
+                </View>
+            )}
+            keyExtractor={(item) => item._id}
+            />
+    </View>
+ )
 }
 
 export default RestaurantScreen;
